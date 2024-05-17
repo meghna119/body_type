6b4163100bca54eff0b6db30599b38bd1874f5c7
meghna119 <137194710+meghna119@users.noreply.github.com>
2024-05-17 10:37:25 +0530

    Update body_type.py

--- body_type.py
+++ body_type.py
@@ -18,7 +18,7 @@ def get_image_files(folder):
     return image_files
 
 # Folder containing images
-image_folder = r"images"
+image_folder = r"images/women"
 
 # Output CSV file
 csv_file = 'Women.csv'
